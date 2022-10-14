57f05503215c6b28cd194f88cb376027bade4eaf
Peter <haudotien@gmail.com>
2022-10-15 03:56:33 +0700

    update craco config

--- craco.config.js
+++ craco.config.js
@@ -1,12 +1,13 @@
-const CracoAlias = require("craco-alias");
-const CracoAntDesignPlugin = require("craco-antd");
+const CracoAlias = require('craco-alias');
+const CracoAntDesignPlugin = require('craco-antd');
 
-const fs = require("fs");
-const path = require("path");
+
+const fs = require('fs');
+const path = require('path');
 
 const rawVar = fs.readFileSync(
-  path.join(__dirname, "src/theme/vars.scss"),
-  "utf8"
+  path.join(__dirname, 'src/theme/vars.scss'),
+  'utf8'
 );
 
 const vars = rawVar
@@ -14,19 +15,20 @@ const vars = rawVar
   .filter((line) => line.length > 0 && !line.startsWith('//'))
   .map((line) => line.split(": "))
   .map(([key, value]) => ({
-    key: key.replace("$", "@"),
-    value: value.replace(";", ""),
+    key: key.replace('$', '@'),
+    value: value.replace(';', ''),
   }))
   .reduce((obj, { key, value }) => ({ ...obj, [key]: value }), {});
-
+console.log('varsss', vars);
 module.exports = {
+  cache: false,
   plugins: [
     {
       plugin: CracoAlias,
       options: {
-        source: "tsconfig",
-        baseUrl: "./",
-        tsConfigPath: "./tsconfig.paths.json",
+        source: 'tsconfig',
+        baseUrl: './',
+        tsConfigPath: './tsconfig.paths.json',
       },
     },
     {
