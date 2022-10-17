43f07671b12a68c8a49bb32ff1cf8dcf3072e78d
Peter <haudotien@gmail.com>
2022-10-17 23:26:33 +0700

    update eslint prettier husky

--- craco.config.js
+++ craco.config.js
@@ -1,39 +1,38 @@
-const CracoAlias = require("craco-alias");
-const CracoAntDesignPlugin = require("craco-antd");
+const CracoAlias = require('craco-alias')
+const CracoAntDesignPlugin = require('craco-antd')
 
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs')
+const path = require('path')
 
-const rawVar = fs.readFileSync(
-  path.join(__dirname, "src/theme/vars.scss"),
-  "utf8"
-);
+const rawVar = fs.readFileSync(path.join(__dirname, 'src/theme/vars.scss'), 'utf8')
 
 const vars = rawVar
-  .split("\r\n")
+  .replace(/\/\*[\s\S]*?\*\/|\/\/.*/g, '')
+  .split('\n')
   .filter((line) => line.length > 0)
-  .map((line) => line.split(": "))
+  .map((line) => line.split(': '))
   .map(([key, value]) => ({
-    key: key.replace("$", "@"),
-    value: value.replace(";", ""),
+    key: key.replace('$', '@'),
+    value: value.replace(';', '')
   }))
-  .reduce((obj, { key, value }) => ({ ...obj, [key]: value }), {});
-
+  .reduce((obj, { key, value }) => ({ ...obj, [key]: value }), {})
+console.log('varsss', vars)
 module.exports = {
+  cache: false,
   plugins: [
     {
       plugin: CracoAlias,
       options: {
-        source: "tsconfig",
-        baseUrl: "./",
-        tsConfigPath: "./tsconfig.paths.json",
-      },
+        source: 'tsconfig',
+        baseUrl: './',
+        tsConfigPath: './tsconfig.paths.json'
+      }
     },
     {
       plugin: CracoAntDesignPlugin,
       options: {
-        customizeTheme: vars,
-      },
-    },
-  ],
-};
+        customizeTheme: vars
+      }
+    }
+  ]
+}
